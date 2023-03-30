9e4d5d76b7ed06a8603a00210381253f0e72e636
Maria-Gerona <83423055+Maria-Gerona@users.noreply.github.com>
2023-03-31 00:12:57 +0300

    You Can't Code Under Pressure #1

---
+++ YouCantCodeUnderPressureNumberOne.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Maria Reina GM on 3/31/2023.
+//
+#include <cstdint>
+
+int32_t double_integer(int32_t n)
+{
+    // We need solution fast !!!
+    int number{};
+    number = n * 2;
+
+    return number;
+}
